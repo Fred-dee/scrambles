6ac3f76b8ed24a1bbed6ab7ff30d46b196744fe9
Fred.Dilapisho <fred.dilapisho@gmail.com>
2019-12-26 22:55:30 +0200

    - tests still failing, misunderstood the question

--- modules/app/src/main/java/fred.dilapisho/Scramblies.java
+++ modules/app/src/main/java/fred.dilapisho/Scramblies.java
@@ -1,18 +1,36 @@
 package fred.dilapisho;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Scramblies {
-	public static boolean scramble(String str1, String str2) {
-		final List<Character> matches = new ArrayList<>();
-		str1.chars().forEachOrdered(
-				charInStr1 -> {
-					if (str2.indexOf((char) charInStr1) != -1 && !matches.contains((char) charInStr1)) {
-						matches.add((char) charInStr1);
-					}
-				}
-		);
-		return matches.size() == str2.length();
-	}
+    public static boolean scramble(String str1, String str2) {
+        boolean returnValue = true;
+        Map<String, Long> charCountStr1 = str1.codePoints().
+                mapToObj(Character::toString)
+                .collect(Collectors.
+                        groupingBy(Function.identity(), Collectors.counting())
+                );
+
+        Map<String, Long> charCountStr2 = str2.codePoints().
+                mapToObj(Character::toString)
+                .collect(Collectors.
+                        groupingBy(Function.identity(), Collectors.counting())
+                );
+        if (charCountStr2.size() < charCountStr1.size()) {
+            return false;
+        } else {
+            for (Map.Entry<String, Long> entry : charCountStr2.entrySet()) {
+                String s = entry.getKey();
+                Long aLong = entry.getValue();
+                if (charCountStr2.containsKey(s) && charCountStr2.get(s) < aLong) {
+                    returnValue = false;
+                    break;
+                }
+            }
+        }
+        return returnValue;
+    }
 }
